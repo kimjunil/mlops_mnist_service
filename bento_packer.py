3e2026842e503e6de31f611e208789b64b0576e0
Kim Junil <iam@junil.kim>
2021-11-09 21:33:24 +0900

    fix model path env

--- bento_packer.py
+++ bento_packer.py
@@ -4,20 +4,31 @@ import h5py
 from tensorflow.python.lib.io import file_io
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--model_path", required=True, type=str)
-args = parser.parse_args()
+def get_args():
+    parser = argparse.ArgumentParser()
+  
+    parser.add_argument(
+        '--model_path',
+        type=str,
+        required=True)
 
-print(args.model_path)
+    args = parser.parse_args()
+    return args
 
-with file_io.FileIO(args.model_path, mode='rb') as model_file:
-     model_gcs = h5py.File(model_file, 'r')
-     model = load_model(model_gcs)
+def main():
+    args = get_args()
+    model_path = args.model_path
+    print("model path :", model_path)
 
-print(model)
+    with file_io.FileIO(args.model_path, mode='rb') as model_file:
+        model_gcs = h5py.File(model_file, 'r')
+        model = load_model(model_gcs)
 
-mnist_svc = MnistService()
-mnist_svc.pack("model", model)
+    mnist_svc = MnistService()
+    mnist_svc.pack("model", model)
 
-saved_path = mnist_svc.save()
-print(f"saved_path : {saved_path}")
+    saved_path = mnist_svc.save()
+    print(f"saved_path : {saved_path}")
+
+if __name__ == '__main__':
+  main()
