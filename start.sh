d0b9e21eac5381cb21669bde4aa61f4fcb1803bb
Kim Junil <iam@junil.kim>
2021-11-25 10:37:08 +0900

    update host

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo $MODEL_PATH
 python bento/bento_packer.py --model_path $MODEL_PATH
 SAVED_PATH=$(bentoml get MnistService:latest --print-location --quiet)
 echo $SAVED_PATH
-curl -X POST -H 'Content-type: application/json' --data '{"text":"배포가 완료되었습니다.\n<http://34.72.79.35/|Swagger>"}' $SLACK_WEBHOOK_URL
+curl -X POST -H 'Content-type: application/json' --data '{"text":"배포가 완료되었습니다.\n<http://35.239.83.18/|Swagger>"}' $SLACK_WEBHOOK_URL
 bentoml serve $SAVED_PATH
